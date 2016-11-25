5f285727a434e4a0303f346efef5fc83eeb3869f
Krzysztof Frańczak <krzysztof.franczak@coi.gov.pl>
2016-11-25 11:33:40 +0100

    Public go...

---
+++ src/test/java/pl/gov/coi/szkolenia/calk/CalculatorTest.java
@@ -0,0 +1,42 @@
+package pl.gov.coi.szkolenia.calk;
+
+import pl.gov.coi.szkolenie.calk.Calculator;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testMnoz() {
+
+        int a = 4;
+        int b = 0;
+
+        Calculator calc = new Calculator();
+
+        int result = calc.mnoz(a, b);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testMnozJeden() {
+
+        int a = 4;
+        int b = 1;
+
+        Calculator calc = new Calculator();
+
+        int result = calc.mnoz(a, b);
+
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void testSubstract(){
+        Calculator calc = new Calculator();
+        assertEquals(1,calc.substract(2,1));
+    }
+
+}

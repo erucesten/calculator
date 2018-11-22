fc2e925cbad1734fe23ca5214a9347817b19e25a
Krzysztof Frańczak <erucesten@gmail.com>
2018-11-22 13:46:20 +0100

    Test 1 Commitu

--- src/main/java/pl/gov/coi/szkolenie/calk/Calculator.java
+++ src/main/java/pl/gov/coi/szkolenie/calk/Calculator.java
@@ -4,12 +4,15 @@ package pl.gov.coi.szkolenie.calk;
  * Created by kfranczak on 2016-11-24.
  */
 public class Calculator {
+
     public int mnoz(int a, int b) {
         return a*b;
     }
+
     public int substract(int a, int b) {
         return a-b;
     }
+
     public int dzielenie(int a, int b)  {
         return a/b;
     }

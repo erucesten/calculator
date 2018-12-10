942ab2477d126b907a9f7aea90ac0bd91b3eee8e
FakeName <krzysztof.franczak@akmf.pl>
2018-12-10 10:39:32 +0100

    Test
    Signed-off-by: FakeName <krzysztof.franczak@akmf.pl>

--- src/main/java/pl/gov/coi/szkolenie/calk/Calculator.java
+++ src/main/java/pl/gov/coi/szkolenie/calk/Calculator.java
@@ -17,12 +17,8 @@ public class Calculator {
         return a/b;
     }
 
-<<<<<<< HEAD
-    public int dzielenieBy1000(int a)  {
-        return a/1000;
-=======
-    public int dzielenieBy100(int a)  {
-        return a/100;
->>>>>>> 4b74490cf19f3275807b21937d10664ea5a8ed4e
+
+    public int dzielenieBy1000(int a) {
+        return a / 1000;
     }
 }
